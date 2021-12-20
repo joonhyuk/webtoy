6e330c7ee7cf92f0cb42a59754ca4abfe5adc9d8
Joonhyuk Choi <joonhyuk@me.com>
2021-12-20 21:33:09 +0900

    fixed?

--- js/cp949coder.js
+++ js/cp949coder.js
@@ -4,7 +4,7 @@ function getQueryString(key) {
     var vars = query.split('&');
     for (var i = 0; i < vars.length; i++) {
         var pair = vars[i].split('=');
-        if (pair[0] === key) {
+        if ((pair[0] === key) && (pair[1] != '')) {
             return decodeURIComponent(pair[1]);
         }
     }
