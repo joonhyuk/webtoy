7f9318b561d553c628a69082ef1458aa78344867
Joonhyuk Choi <joonhyuk@me.com>
2022-02-18 12:08:10 +0900

    chapter 3-09

--- flask/pybo/views/auth_views.py
+++ flask/pybo/views/auth_views.py
@@ -1,3 +1,4 @@
+import functools
 from flask import Blueprint, url_for, render_template, flash, request, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
@@ -55,3 +56,12 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('main.index'))
+
+def login_required(view):
+    """데코레이터 함수 만들기. 다른 함수에 @login_required 애너테이션을 지정하면 login_required가 실행된다"""
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
--- flask/pybo/views/question_views.py
+++ flask/pybo/views/question_views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, g
+from .auth_views import login_required
 from pybo.models import Question
 from ..forms import QuestionForm, AnswerForm
 
@@ -9,11 +10,12 @@ from .. import db
 bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/create/', methods=('GET', 'POST'))
+@login_required
 def create():
     form = QuestionForm()
     print(form.validate_on_submit())
     if request.method == 'POST' and form.validate_on_submit():
-        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now())
+        question = Question(subject=form.subject.data, content=form.content.data, create_date=datetime.now(), user=g.user)
         db.session.add(question)
         db.session.commit()
         return redirect(url_for('main.index'))
