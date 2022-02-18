779547065e28f51f4f7d8997531c5f839402389d
Joonhyuk Choi <joonhyuk@me.com>
2022-02-18 12:49:36 +0900

    chapter 3-10 modify & delete

--- flask/pybo/models.py
+++ flask/pybo/models.py
@@ -1,4 +1,3 @@
-from sqlalchemy import false, true
 from pybo import db
 
 class Question(db.Model):
@@ -6,6 +5,7 @@ class Question(db.Model):
     subject = db.Column(db.String(200), nullable = False)
     content = db.Column(db.Text(), nullable = False)
     create_date = db.Column(db.DateTime(), nullable = False)
+    modify_date = db.Column(db.DateTime(), nullable = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref = db.backref('question_set'))
     
@@ -15,6 +15,7 @@ class Answer(db.Model):
     question = db.relationship('Question', backref=db.backref('answer_set', cascade='all, delete-orphan'))
     content = db.Column(db.Text(), nullable = False)
     create_date = db.Column(db.DateTime(), nullable = False)
+    modify_date = db.Column(db.DateTime(), nullable = True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref = db.backref('answer_set'))
     
