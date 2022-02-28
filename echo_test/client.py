948ba5013cde7896bab147ae528995e0c1c1b11c
Joonhyuk Choi <joonhyuk@me.com>
2022-02-28 14:15:33 +0900

    echo server-client test

--- echo_test/client.py
+++ echo_test/client.py
@@ -1,16 +1,20 @@
 import socket
 addr=("127.0.0.1",4444)
 
+cnt = 0
+
 with socket.socket() as s:	#소켓 생성
     s.connect(addr)		#소켓 연결 요청
     while(1):		#무한 반복
         str1=input("echo: ")	#사용자에게 입력받음
-        if(str1 in ('exit', 'quit')):	#finished가 입력되면
+        # str1 = str(cnt)
+        if(str1 in ('exit', 'quit', 'shutdown')):	#finished가 입력되면
             s.send(str1.encode())	#메시지 encoding해서 전송
             s.close()			#소켓 닫음
             break
         s.send(str1.encode())	#메시지 encoding 후 전송
         data=s.recv(1024)		#server로부터 받은 메시지 data에 저장
         print(data.decode())    #data decoding후 출력
+        # cnt += 1
 
     print("program finish")
--- echo_test/server.py
+++ echo_test/server.py
@@ -13,7 +13,7 @@ with socket.socket() as s:	#소켓 할당
     running = True
     while(running):	#무한반복
         data=conn.recv(1024)	#client가 보낸 메시지를 data에 저장
-        if(data.decode() in ('quit', 'exit')):	#data decoding 결과가 finished면
+        if(data.decode() in ('shutdown', )):	#data decoding 결과가 finished면
             running = False	#반복문 탈출
         conn.send(data)			#data를 그대로 client에게 전송
         print(data.decode())	#보낸 데이터 읽기
